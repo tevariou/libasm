ac8f911dc2625654778f472c9d7cfa32b8cb07f4
Teva RIOU <teva@yusofleet.com>
2020-08-20 15:10:56 +0200

    fix: stack alignement

--- srcs/ft_strdup.s
+++ srcs/ft_strdup.s
@@ -5,16 +5,21 @@
 	section .text
 
 _ft_strdup:
+	enter 0, 0
 	call _ft_strlen
 	inc rax
-	mov rsi, rdi
+	push rdi
 	mov rdi, rax
+	sub rsp, 8
 	call _malloc
+	add rsp, 8
+	pop rsi
 	test rax, rax
 	jz exit
 	mov rdi, rax
 	call _ft_strcpy
 
 exit:
+	leave
 	ret
 
