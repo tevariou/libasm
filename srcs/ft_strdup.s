ce786b1dfbcf1a12c745730a0537424e10e42f5f
Teva RIOU <teva@yusofleet.com>
2020-08-22 11:52:00 +0200

    comment

--- srcs/ft_strdup.s
+++ srcs/ft_strdup.s
@@ -5,13 +5,13 @@
 	section .text
 
 _ft_strdup:
+    push rdi ; save src
 	call _ft_strlen
-	inc rax
-	push rdi
+	inc rax ; increment for null termination
 	mov rdi, rax
 	call _malloc
-	pop rsi
-	test rax, rax
+	pop rsi ;
+	test rax, rax ; check malloc return value
 	jz exit
 	mov rdi, rax
 	call _ft_strcpy
