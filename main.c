5f8baef6d939933d6478e38cccc83b2565dbd6eb
Teva RIOU <teva.riou@gmail.com>
2020-08-23 16:28:22 +0200

    testing

--- main.c
+++ main.c
@@ -3,24 +3,26 @@
 #include <string.h>
 #include <fcntl.h>
 #include <unistd.h>
+#include <stdio.h>
 
 int		main()
 {
 	char *s;
 	char buf[42];
 	int file;
-	ssize_t ret;
 
     if ((file = open("./Makefile", O_RDONLY)) < 0)
         return (EXIT_FAILURE);
     memset(buf, 0, 42);
-    while ((ret = ft_read(file, buf, 41)) > 0)
+    while (ft_read(file, buf, 41) > 0)
     {
         s = ft_strdup(buf);
         ft_write(1, s, ft_strlen(s));
         free(s);
         memset(buf, 0, 42);
     }
+    if (errno)
+        perror(NULL);
     close(file);
 	return (EXIT_SUCCESS);
 }
